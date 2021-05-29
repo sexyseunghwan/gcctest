a2b9f066eabb968d8fb26332dc1dad2cbcba36fe
seung hwan shin <67136763+sexyseunghwan@users.noreply.github.com>
2021-05-29 23:11:08 +0900

    2021-05-29

---
+++ testtest.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+
+struct ST {
+    char c1;//4
+    char c2;//4
+    char c3;//
+    int age;//1;;
+
+};
+
+int main(){
+
+
+    printf("%d",sizeof(struct ST));
+
+
+    return 0;
+
+}
